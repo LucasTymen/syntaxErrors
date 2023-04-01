ace3ff3b41ce1743e33268b2deaed18bc8cd41d5
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-01 16:26:51 +0200

    typeErrors

--- TypeErrors/files/script.py
+++ TypeErrors/files/script.py
@@ -1,4 +1,25 @@
 
+# Area Calculator 📏
+
+import math
+
+base = 20
+height = 30
+area = base * height / 2
+
+print("The triangle area is", area)
+
+length = 2
+width = 12
+area = length * width
+
+print("The rectangle area is",  area)
+
+radius = 36
+area = math.pi * radius * radius
+
+print("The circle area is", area)
+
 """
 ################### original code : ######################
 # Area Calculator 📏
@@ -21,4 +42,11 @@ radius = 36
 area = math.pi * radius * radius
 
 print("The circle area is", area)
+
+=====>>>> the code strikes the error :
+
+  File "script.py", line 15
+    print("The rectangle area is"  area )
+                                      ^
+SyntaxError: invalid syntax
 """
